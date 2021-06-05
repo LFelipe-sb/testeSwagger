44797dd3d9e30f51f077b0d009c7ea6a34a67648
LFelipe-sb <luisfelipe.sb@outlook.com>
2021-06-05 15:58:13 -0300

    Com swagger Okay

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import routes from './routes.js';
 import cors from 'cors';
-// import swaggerUI from 'swagger-ui-express';
-// import apiDoc from './api-doc.json';
+import swaggerUI from 'swagger-ui-express';
+import apiDoc from './api-doc.json';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', routes);
-// app.use('/docs', swaggerUI.serve, swaggerUI.setup(apiDoc));
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(apiDoc));
 
 app.get('/', (req, res) => {
     res.send({
